b8ee3830492861dd6e30d42acb24dab296c161ff
Bryan Silva <bryan.liaris@sptech.com>
2023-12-10 12:22:33 -0300

    aplicando validação de login

--- src/main/java/AppPerformee.java
+++ src/main/java/AppPerformee.java
@@ -1,5 +1,6 @@
 import Dao.DaoDados;
 import Modelo.*;
+import Slack.SlackConfig;
 
 import java.util.Scanner;
 
@@ -9,129 +10,161 @@ public class AppPerformee {
 
         DaoDados dao = new DaoDados();
         Scanner leitor = new Scanner(System.in);
+        Scanner leitorString = new Scanner(System.in);
 
         Integer opcao = 0;
         String ipServidor;
         Integer numTentativas = 6;
 
-
-        System.out.println("""
-                +-------------------------------+
-                |   Bem vindo ao \033[1;34mperformee.\033[m     |""");
-
-
+        Integer opcaoLogin;
         do {
-
-            System.out.print("""
+            System.out.println("""
                     +-------------------------------+
-                    Digite o Ip do Servidor:""");
-            ipServidor = leitor.nextLine();
+                    |   Login \033[1;34mperformee.\033[m            |""");
 
-            Boolean validacao = dao.buscarIp(ipServidor);
+            System.out.println("""
+                    +-------------------------------+
+                    | \033[1;35m1)\033[m Login Como Usuário         |
+                    | \033[1;35m2)\033[m Login Como Admin           |
+                    | \033[1;35m3)\033[m Sair                       |
+                    +-------------------------------+""");
+            opcaoLogin = leitor.nextInt();
 
-            if (!validacao) {
-                System.out.println("Servidor não Encontrado!");
-                numTentativas--;
+            switch (opcaoLogin) {
+                case 1, 2: {
+                    System.out.println("Digite o email:");
+                    String email = leitorString.nextLine();
 
-                if (numTentativas == 0) {
-                    System.out.println("\033[1;31mAcabou suas tentativas! Volte mais tarde\033[m");
-                    String descricao = """
-                    : Usuário do IP %s, hostName: %s. Esgotou o número máximo de tentativas para acessar o servidor! e o JAR foi encerrado""".formatted(dao.getIpUser(), dao.getHostNameUser(), ipServidor);
-                    dao.setLog(descricao);
+                    System.out.println("Digite a senha:");
+                    String senha = leitorString.nextLine();
+
+                    dao.verificarLogin(opcaoLogin, email, senha);
+                    break;
+                }
+                case 3: {
+                    System.out.println("Saindo...");
                     System.exit(0);
                 }
-                System.out.println("""
-                        Você tem \033[1;31m%d\033[m tentativas!""".formatted(numTentativas));
-            } else {
-                System.out.println("\033[1;32mServidor encontrado!\033[m");
-                do {
-
-                    System.out.println("""
-                            +-------------------------------+
-                            | \033[1;35m1)\033[m Cadastrar componentes      |
-                            | \033[1;35m2)\033[m Atualizar componentes      |
-                            | \033[1;35m3)\033[m Inserir dados de leitura   |
-                            | \033[1;35m4)\033[m Ver Componentes            |
-                            | \033[1;35m5)\033[m Ver Leituras               |
-                            | \033[1;35m6)\033[m Sair                       |
-                            +-------------------------------+""");
-
-                    opcao = leitor.nextInt();
-
-
-                    switch (opcao) {
-                        case 1: {
-                            dao.inserirComponente();
-                            break;
-                        }
-                        case 2: {
-                            Integer opcaoAtualizar;
-                            do {
-                                System.out.println("""
-                                        +--------------------------------------+
-                                        | Qual componente deseja atualizar?    |
-                                        +--------------------------------------+
-                                        | \033[1;35m1)\033[m Atualizar CPU                     |
-                                        | \033[1;35m2)\033[m Atualizar RAM                     |
-                                        | \033[1;35m3)\033[m Atualizar Disco                   |
-                                        | \033[1;35m4)\033[m Atualizar Rede                    |
-                                        | \033[1;35m5)\033[m Cancelar                          |
-                                        +--------------------------------------+""");
-                                opcaoAtualizar = leitor.nextInt();
-
-                                dao.atualizarComponete(opcaoAtualizar);
-                            } while (opcaoAtualizar != 5);
-                            break;
-                        }
-                        case 3: {
-                            dao.inserirLeitura();
-                            break;
-                        }
-                        case 4: {
-                            System.out.println("""
-                                    +----------------------------+
-                                    | Componentes:               |
-                                    +----------------------------+""");
-                            for (Componentes comp : dao.exibirComponentes()) {
-                                System.out.println(comp);
-                            }
-                            break;
-                        }
-                        case 5: {
-                            System.out.println("""
-                                    +----------------------------+
-                                    | Leituras:                  |
-                                    +----------------------------+""");
-                            for (Cpu cpu : dao.exibirCpu()) {
-                                System.out.println(cpu);
-                            }
-                            for (Ram ram : dao.exibirRam()) {
-                                System.out.println(ram);
-                            }
-                            for (Disk disco : dao.exibirDisco()) {
-                                System.out.println(disco);
-                            }
-                            for (Rede rede : dao.exibirRede()) {
-                                System.out.println(rede);
-                            }
-
-
-                            break;
-                        }
-                        case 6: {
-                            System.out.println("""
-                                    Saindo...""");
-                            String descricao = """
-                    : Usuário do IP %s, hostName: %s. Saiu do servidor de IP: %s""".formatted(dao.getIpUser(), dao.getHostNameUser(), ipServidor);
-                            dao.setLog(descricao);
-                            System.exit(0);
-                        }
-                        default: {
-                            System.out.println("Opção \033[1;31minválida!\033[m digite novamente");
-                        }
-                    }
-                } while (opcao != 3);
+                default: {
+                    System.out.println("Opção \033[1;31minválida!\033[m");
+                }
             }
-        } while (opcao != 3);
+
+        } while (!dao.getLoginPermition().equals(true));
+
+
+        if (dao.getLoginPermition().equals(true)) {
+
+            do {
+                System.out.print("""
+                        +----------------------------------------------+
+                        Digite o Ip do Servidor:""");
+                ipServidor = leitorString.nextLine();
+
+                Boolean validacao = dao.buscarIp(ipServidor);
+
+                if (!validacao) {
+                    System.out.println("Servidor não Encontrado!");
+                    numTentativas--;
+
+                    if (numTentativas == 0) {
+                        System.out.println("\033[1;31mAcabou suas tentativas! Volte mais tarde\033[m");
+                        String descricao = """
+                                : Usuário do IP %s, hostName: %s. Esgotou o número máximo de tentativas para acessar o servidor! e o JAR foi encerrado""".formatted(dao.getIpUser(), dao.getHostNameUser(), ipServidor);
+                        dao.setLog(descricao);
+                        System.exit(0);
+                    }
+                    System.out.println("""
+                            Você tem \033[1;31m%d\033[m tentativas!""".formatted(numTentativas));
+                } else {
+                    System.out.println("\033[1;32mServidor encontrado!\033[m");
+                    do {
+
+                        System.out.println("""
+                                +-------------------------------+
+                                | \033[1;35m1)\033[m Cadastrar componentes      |
+                                | \033[1;35m2)\033[m Atualizar componentes      |
+                                | \033[1;35m3)\033[m Inserir dados de leitura   |
+                                | \033[1;35m4)\033[m Ver Componentes            |
+                                | \033[1;35m5)\033[m Ver Leituras               |
+                                | \033[1;35m6)\033[m Sair                       |
+                                +-------------------------------+""");
+
+                        opcao = leitor.nextInt();
+
+
+                        switch (opcao) {
+                            case 1: {
+                                dao.inserirComponente();
+                                break;
+                            }
+                            case 2: {
+                                Integer opcaoAtualizar;
+                                do {
+                                    System.out.println("""
+                                            +--------------------------------------+
+                                            | Qual componente deseja atualizar?    |
+                                            +--------------------------------------+
+                                            | \033[1;35m1)\033[m Atualizar CPU                     |
+                                            | \033[1;35m2)\033[m Atualizar RAM                     |
+                                            | \033[1;35m3)\033[m Atualizar Disco                   |
+                                            | \033[1;35m4)\033[m Atualizar Rede                    |
+                                            | \033[1;35m5)\033[m Cancelar                          |
+                                            +--------------------------------------+""");
+                                    opcaoAtualizar = leitor.nextInt();
+
+                                    dao.atualizarComponete(opcaoAtualizar);
+                                } while (opcaoAtualizar != 5);
+                                break;
+                            }
+                            case 3: {
+                                dao.inserirLeitura();
+                                break;
+                            }
+                            case 4: {
+                                System.out.println("""
+                                        +----------------------------+
+                                        | Componentes:               |
+                                        +----------------------------+""");
+                                for (Componentes comp : dao.exibirComponentes()) {
+                                    System.out.println(comp);
+                                }
+                                break;
+                            }
+                            case 5: {
+                                System.out.println("""
+                                        +----------------------------+
+                                        | Leituras:                  |
+                                        +----------------------------+""");
+                                for (Cpu cpu : dao.exibirCpu()) {
+                                    System.out.println(cpu);
+                                }
+                                for (Ram ram : dao.exibirRam()) {
+                                    System.out.println(ram);
+                                }
+                                for (Disk disco : dao.exibirDisco()) {
+                                    System.out.println(disco);
+                                }
+                                for (Rede rede : dao.exibirRede()) {
+                                    System.out.println(rede);
+                                }
+                                break;
+                            }
+                            case 6: {
+                                System.out.println("""
+                                        Saindo...""");
+                                String descricao = """
+                                        : Usuário do IP %s, hostName: %s. Saiu do servidor de IP: %s""".formatted(dao.getIpUser(), dao.getHostNameUser(), ipServidor);
+                                dao.setLog(descricao);
+                                System.exit(0);
+                            }
+                            default: {
+                                System.out.println("Opção \033[1;31minválida!\033[m digite novamente");
+                            }
+                        }
+                    } while (opcao != 3);
+                }
+            } while (opcao != 3);
+        }
     }
 }
